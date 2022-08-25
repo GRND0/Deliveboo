ea54dcf7a33f0eb06ee0ad4d28287f1735f62b73
GRND0 <zefiro.turchetta@gmail.com>
2022-08-25 15:57:14 +0200

    creati e popolati file migration. Eseguita prima migration

---
+++ database/migrations/2022_08_25_132530_create_dishes_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDishesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dishes', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->text('ingredients')->nullable();
+            $table->double('price', 5 ,2)->unsigned()->default(0);
+            $table->bigInteger('restaurant_id')->unsigned();
+            $table->text('image')->nullable();
+            $table->boolean('avaible')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dishes');
+    }
+}
---
+++ database/migrations/2022_08_25_132539_create_orders_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->double('total_price' , 6 ,2)->unsigned()->default(0);
+            $table->bigInteger('restaurant_id')->unsigned();
+            $table->boolean('payment')->default(false);
+            $table->dateTime('date');
+            $table->string('customer_name');
+            $table->string('customer_lastname');
+            $table->string('customer_phone')->unique();
+            $table->string('customer_address');
+            $table->string('customer_email')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
