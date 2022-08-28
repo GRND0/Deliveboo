bc0cade13e64424f4ff3f1e30a4badcd07357b29
DavideIsaia <davide.enjoy@hotmail.it>
2022-08-28 11:09:55 +0200

    aggiunto footer e stile header e footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,71 @@
 <footer>
-  FOOTER
+  <div class="container py-3">
+    {{-- footer nav  --}}
+    <div class="nav-footer row g-5 justify-content-around">
+      <div class="col-md-6 p-2 col-lg-3">
+        <div class="footer-card">
+          <h5>Scopri Deliveboo</h5>
+          <ul>
+            <li><a href="">Investitori</a></li>
+            <li><a href="">Chi siamo</a></li>
+            <li><a href="">Ristoranti</a></li>
+            <li><a href="">Altro</a></li>
+            <li><a href="">Pressroom</a></li>
+            <li><a href="">Il nostro blog</a></li>
+            <li><a href="">Programmazione</a></li>
+            <li><a href="">Design</a></li>
+            <li><a href="">Lavora con noi</a></li>
+            <li><a href="">Diventa nostro partner</a></li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="col-md-6 p-2  col-lg-3">
+        <div class="footer-card">
+          <h5>Note legali</h5>
+          <ul>
+            <li><a href="">Termini & condizioni</a></li>
+            <li><a href="">Informativa sulla privacy</a></li>
+            <li><a href="">Cookies</a></li>
+          </ul>
+        </div>          
+      </div>
+
+      <div class="col-md-6 p-2  col-lg-3">
+        <div class="footer-card">
+          <h5>Aiuto</h5>
+          <ul>
+            <li><a href="">Contatti</a></li>
+            <li><a href="">FAQ</a></li>
+            <li><a href="">Tipi di cucina</a></li>
+          </ul>
+        </div>          
+      </div>
+
+      <div class="col-md-6 p-2 col-lg-3">
+        <div class="footer-card">
+          @auth
+            <h5>Porta Deliveboo con te</h5>
+            <img class="app-store" src="" alt="apple store logo">
+            <img class="app-store" src="" alt="play store logo">
+          @else
+            <h5>Diventa nostro partner</h5>
+            <ul>
+                <li class="subscribe"><a class="register-btn" href="{{route('register')}}">Iscriviti</a></li>
+            </ul>
+          @endauth
+        </div>
+      </div>
+    </div>
+    
+    {{-- social channels  --}}
+    <nav class="social-channels">
+      <ul>
+        <li><a href=""><img src="" alt="facebook logo"></a></li>
+        <li><a href=""><img src="" alt="twitter logo"></a></li>
+        <li><a href=""><img src="" alt="instagram logo"></a></li>
+      </ul>
+      <p>2022 DeliveBoo - Classe 62 Team 3 with &hearts;</p>
+    </nav>
+  </div>
 </footer>
--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -15,15 +15,15 @@
         {{-- voci menu --}}
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div class="navbar-nav d-flex align-items-end hamb-menu-clicked">
-            <a class="nav-link" href="#">Collabora con noi</a>
-            <a class="nav-link" href="#">Menu</a>
+            <a class="nav-link" href="">Collabora con noi</a>
+            <a class="nav-link" href="">Menu</a>
 
             @auth
               {{-- voci menu visibili se loggati --}}
-              <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+              <a class="nav-link" href="">
                 {{ Auth::user()->name }}
               </a>
-              <a class="btn btn-return-restaurant" href="">Ritorna al tuo ristorante</a>
+              <a class="btn btn-info" href="">Ritorna al tuo ristorante</a>
               <a class="btn btn-danger exit-btn" href="{{ route('logout') }}"onclick="event.preventDefault(); 
                 document.getElementById('logout-form').submit();"
                 > {{ __('Logout') }} </a>
@@ -33,7 +33,7 @@
               
               @else
               {{-- voce menu visibile se non loggati --}}
-              <a class="register-btn" href="{{ route('register') }}">Diventa nostro partner</a>
+              <a class="nav-link" href="{{ route('register') }}">Diventa nostro partner</a>
             @endauth
           </div>
         </div>
