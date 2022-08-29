0cde002fbab8d5398c3ea57ee918e3bfc7b499aa
DavideIsaia <davide.enjoy@hotmail.it>
2022-08-29 16:40:50 +0200

    visualizzazione info user e restaurant

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,7 +1,19 @@
 @extends('layouts.main')
 @section('content')
 
-<h2>Ciao </h2>
+<h2>Ciao {{$user->name}}. Questa è la home di amministrazione </h2>
+
+<div class="col">
+    <div class="card" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">{{$user->restaurant_name}}</h5>
+          <p class="card-text">Qui puoi gestire la tua attività</p>
+          <img src="storage/{{ $user->image }}" alt="">
+        </div>
+      </div>
+    
+</div> 
+
 
 
 @endsection
